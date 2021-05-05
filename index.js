e7d08b3587a977323ae1acf715b1106bd62e4ba7
Sunil Kumar <sunil32.kumar@paytm.com>
2021-05-05 11:27:12 +0530

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express(), port = process.env.PORT || 3030;
 
+app.get('/testing', (req, res) => {
+    res.status(200).send('Testing route handled');
+});
+
 app.get('/', (req, res) => {
     res.status(200).send('Server is running...');
 });
+
 
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`)
